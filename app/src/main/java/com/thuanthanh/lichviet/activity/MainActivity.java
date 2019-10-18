10f1b6455b21bda39b08e326a51df16f9a56959d
hoangrapperIT <nguyenxuanhoang02081998@gmail.com>
2019-10-18 10:11:24 +0700

    giao dien tu vi - hoang

--- app/src/main/java/com/thuanthanh/lichviet/activity/MainActivity.java
+++ app/src/main/java/com/thuanthanh/lichviet/activity/MainActivity.java
@@ -1,5 +1,6 @@
 package com.thuanthanh.lichviet.activity;
 
+
 import android.os.Build;
 import android.os.Bundle;
 import android.view.MenuItem;
@@ -7,14 +8,20 @@ import android.view.Window;
 import android.view.WindowManager;
 
 
+
+
 import androidx.annotation.NonNull;
+
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
 
+
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.thuanthanh.lichviet.R;
+
 import com.thuanthanh.lichviet.fragment.HomeFragment;
 import com.thuanthanh.lichviet.fragment.LichVN_Fragment;
+import com.thuanthanh.lichviet.fragment.TuviFragment;
 
 public class MainActivity extends BaseActivity {
     private BottomNavigationView bottomnavigation;
@@ -23,6 +30,7 @@ public class MainActivity extends BaseActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         bottomnavigation = findViewById(R.id.bottom_navigation);
         bottomnavigation.setOnNavigationItemSelectedListener(navListener);
         loadFragment(new HomeFragment());
@@ -43,11 +51,14 @@ public class MainActivity extends BaseActivity {
                     return true;
                 case R.id.nav_vn_calendar:
                     fragment = new LichVN_Fragment();
-                    loadFragment(fragment);
+                    loadFragment( fragment);
                     return  true;
                 case R.id.nav_event:
-                case R.id.nav_tuvi:
 
+                case R.id.nav_tuvi:
+                    fragment=new TuviFragment();
+                    loadFragment(fragment);
+                    return true;
             }
             return false;
         }
@@ -56,9 +67,11 @@ public class MainActivity extends BaseActivity {
     private void loadFragment(Fragment fragment) {
         FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
         transaction.replace(R.id.frame_container, fragment);
-     //   transaction.addToBackStack(null);
+     //  transaction.addToBackStack(null);
         transaction.commit();
 
+
     }
+
 
 }
---
+++ app/src/main/java/com/thuanthanh/lichviet/fragment/TuviFragment.java
@@ -0,0 +1,61 @@
+package com.thuanthanh.lichviet.fragment;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.GridView;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.thuanthanh.lichviet.R;
+
+public class TuviFragment extends Fragment {
+    GridView gridView;
+    String titile[] = {"Tử Vi \n2019", "Tử Vi \n Hằng Ngày", "Văn Khấn", "Xem Sao", "Tử Vi \n Trọn Đời", "Bói \n Tình Duyên", "Giải Mộng", "La Bàn", "Thước \n Lỗ Ban", "Nhịp \n Sinh Học", "Cung \n Hoàng Đạo", "Đổi Ngày"};
+    int img[] = {R.drawable.ic_tuvi_2019, R.drawable.i_tuvihangngay, R.drawable.i_vankhan, R.drawable.i_xemsao, R.drawable.i_tuvitrondoi, R.drawable.i_boitinhduyen, R.drawable.i_giaimong, R.drawable.i_laban, R.drawable.i_thuocloban, R.drawable.i_nhipsinhhoc, R.drawable.i_cunghoangdao, R.drawable.i_doingay};
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_tuvi, container, false);
+        gridView = view.findViewById(R.id.gridview);
+        CustomAdapter customAdapter =new CustomAdapter();
+        gridView.setAdapter(customAdapter);
+        return view;
+
+
+    }
+    private class CustomAdapter extends BaseAdapter{
+
+        @Override
+        public int getCount() {
+           return img.length;
+        }
+
+        @Override
+        public Object getItem(int i) {
+            return null;
+        }
+
+        @Override
+        public long getItemId(int i) {
+            return 0;
+        }
+
+        @Override
+        public View getView(int i, View view, ViewGroup viewGroup) {
+            View view1 =getLayoutInflater().inflate(R.layout.gridview_item,null);
+            ImageView imageView = view1.findViewById(R.id.imgic);
+            TextView textView =view1.findViewById(R.id.tvItem);
+            imageView.setImageResource(img[i]);
+            textView.setText(titile[i]);
+            return view1;
+        }
+    }
+}
