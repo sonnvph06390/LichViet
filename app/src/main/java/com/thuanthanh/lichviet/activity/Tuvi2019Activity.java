b0f127217d56be45a5b95520d56ab57dda739025
hoangrapperIT <nguyenxuanhoang02081998@gmail.com>
2019-10-21 10:36:42 +0700

    update code Tu vi 2019 app lich viet

--- app/src/main/java/com/thuanthanh/lichviet/activity/Tuvi2019Activity.java
+++ app/src/main/java/com/thuanthanh/lichviet/activity/Tuvi2019Activity.java
@@ -1,6 +1,7 @@
 package com.thuanthanh.lichviet.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
 import android.widget.NumberPicker;
@@ -9,28 +10,43 @@ import android.widget.Toast;
 import com.thuanthanh.lichviet.R;
 
 public class Tuvi2019Activity extends AppCompatActivity {
-
+    private Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tuvi2019);
+        toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        toolbar.setTitle("Tử Vi 2019");
+        getSupportActionBar().setDisplayHomeAsUpEnabled(false);
         anhxa();
         NumberPicker picker1 = (NumberPicker) findViewById(R.id.np1);
-        picker1.setMaxValue(2);
-        String[] strs1 = {"Nam","Nữ" };
-        picker1.setDisplayedValues(strs1);
-        picker1.setWrapSelectorWheel(true);
-        NumberPicker numberPicker = findViewById(R.id.np2);
+        if (picker1 != null) {
 
+
+            picker1.setMinValue(0);
+            picker1.setMaxValue(1);
+            final String[] strs1 = {"Nam", "Nữ"};
+            picker1.setDisplayedValues(strs1);
+            picker1.setWrapSelectorWheel(true);
+            picker1.setOnValueChangedListener(new NumberPicker.OnValueChangeListener() {
+                @Override
+                public void onValueChange(NumberPicker numberPicker, int i, int i1) {
+                    String text = "Bạn đã chọn: " + strs1[i1];
+                    Toast.makeText(Tuvi2019Activity.this, text, Toast.LENGTH_SHORT).show();
+                }
+            });
+        }
+        NumberPicker numberPicker = findViewById(R.id.np2);
         if (numberPicker != null) {
-            numberPicker.setMinValue(0);
-            numberPicker.setMaxValue(10);
+            numberPicker.setMinValue(1970);
+            numberPicker.setMaxValue(2003);
             numberPicker.setWrapSelectorWheel(true);
             numberPicker.setOnValueChangedListener(new NumberPicker.OnValueChangeListener() {
                 @Override
                 public void onValueChange(NumberPicker picker, int oldVal, int newVal) {
-                    String text = "Changed from " + oldVal + " to " + newVal;
+                    String text = "Bạn đã chọn: " + newVal;
                     Toast.makeText(Tuvi2019Activity.this, text, Toast.LENGTH_SHORT).show();
                 }
             });
@@ -39,6 +55,5 @@ public class Tuvi2019Activity extends AppCompatActivity {
     }
 
     private void anhxa() {
-
     }
 }
